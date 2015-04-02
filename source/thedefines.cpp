5bc716644c7602cbffa546faab56f0746829de6f
Johan Deimert <johan.deimert@combitech.se>
2015-04-02 15:56:54 +0200

    Commit of source file and Visual Studio 2013 project.

---
+++ source/thedefines.cpp
@@ -0,0 +1,88 @@
+#define vZ a
+#define LW o
+#define ag {
+#define pZ u
+#define hm e
+#define eu i
+#define ju =
+#define rI }
+#define XZS(x) #x
+#include "stdio.h"
+#define uWCS int
+#define vq W
+#define JB C
+#define kx S
+#define Tlb(v,x,D,w) v##x##D##w
+#define BIV(v,x,D,w) Tlb(v,x,D,w)
+BIV(pZ, vq, JB, kx)
+#define hWkO main
+#define PL h
+#define YP k
+#define qe O
+#define KAT(e,R,c,o,i,d,T,I,f) e##R##c##o (  d##T##I##f
+#define SUR(e,R,c,o,i,d,T,I,f) KAT(e,R,c,o,i,d,T,I,f)
+SUR(PL, vq, YP, qe, x, pZ, vq, JB, kx)
+#define EvIX argc
+#define vB E
+#define kJ v
+#define pq I
+#define hJ X
+#define ORv(G,Z,I,M,p) G##Z##I##M ,
+#define abN(G,Z,I,M,p) ORv(G,Z,I,M,p)
+abN(vB, kJ, pq, hJ, x)
+#define oxPl char
+#define fd x
+#define xy P
+#define Vf l
+#define pUo(b,e,J,V) b##e##J##V
+#define ggB(b,e,J,V) pUo(b,e,J,V)
+ggB(LW, fd, xy, Vf)
+#define lVnp argv
+#define zE *
+#define cD V
+#define DF n
+#define hX p
+#define EnK(v,X,K,z,C,c) v X##K##z##C )
+#define mfB(v,X,K,z,C,c) EnK(v,X,K,z,C,c)
+mfB(zE, Vf, cD, DF, hX, x)
+ag
+#define NBc(P,X,b,E) P##X##b##E
+#define IhA(P,X,b,E) NBc(P,X,b,E)
+IhA(LW, fd, xy, Vf)
+#define TU Z
+#define Ch H
+#define xh j
+#define UP s
+#define tu \ 
+#define Bi 0
+#define HW 4
+#define ssD(g,T,J,G,H,N,s,o,U,w,Y,a,I,z,d,f,O,X,x) g T##J G##H##N##s##(##U##w##Y##a##I##z##d##f##O##X##)
+#define NHL(g,T,J,G,H,N,s,o,U,w,Y,a,I,z,d,f,O,X,x) ssD(g,T,J,G,H,N,s,o,U,w,Y,a,I,z,d,f,O,X,x)
+NHL(zE, kJ, TU, ju, hJ, TU, kx, x, Ch, hm, xh, UP, vZ, DF, tu, Bi, HW, Bi, x)
+#define sE T
+#define Wv b
+#define tm .
+#define NUO(O,G,h,q,E,B,a,U,K,s,y) O##G##h##(##E##B##a##U##K##s##)
+#define Dpa(O,G,h,q,E,B,a,U,K,s,y) NUO(O,G,h,q,E,B,a,U,K,s,y)
+Dpa(hJ, TU, kx, x, sE, LW, Wv, Wv, hm, tm, x)
+#define VN y
+#define Gy f
+#define an d
+#define WRJ(i,y,h,M,W,j,K,d,l,N,A,k,V,c,g,b,u,f,m,S,p,C,Y) i##y##h##(##W##j##K##d##l##N##A##k##V##c##g##b##u##f##m##S##p##C##)
+#define CZd(i,y,h,M,W,j,K,d,l,N,A,k,V,c,g,b,u,f,m,S,p,C,Y) WRJ(i,y,h,M,W,j,K,d,l,N,A,k,V,c,g,b,u,f,m,S,p,C,Y)
+CZd(hJ, TU, kx, x, JB, vZ, DF, tu, Bi, HW, Bi, VN, LW, pZ, tu, Bi, HW, Bi, Gy, eu, DF, an, x)
+#define Ro t
+#define gm g
+#define oe ?
+#define ED ;
+#define YvD(P,p,m,L,F,u,w,S,N,M,C,h,s,d,g,W,c,V,f,O,l,G) P##p##m##(##F##u##w##S##N##M##C##h##s##d##g##W##c##V##f##O##)##G
+#define taQ(P,p,m,L,F,u,w,S,N,M,C,h,s,d,g,W,c,V,f,O,l,G) YvD(P,p,m,L,F,u,w,S,N,M,C,h,s,d,g,W,c,V,f,O,l,G)
+taQ(hJ, TU, kx, x, tu, Bi, HW, Bi, Ro, PL, hm, tu, Bi, HW, Bi, Wv, pZ, gm, tu, oe, x, ED)
+#define LbDI printf
+#define dp L
+#define Ys D
+#define Do %
+#define kdn(k,W,E,t,Y,c,S,M,m,p,z,G,Z,R,U,i,N,a,H) k##W##E##t ( c##S##M##(##p##z##G##Z##) ,  i##N ) H
+#define hMi(k,W,E,t,Y,c,S,M,m,p,z,G,Z,R,U,i,N,a,H) kdn(k,W,E,t,Y,c,S,M,m,p,z,G,Z,R,U,i,N,a,H)
+hMi(dp, Wv, Ys, pq, x, hJ, TU, kx, x, Do, UP, tu, DF, x, x, kJ, TU, x, ED)
+rI
